b55603ffc4bad35213157f6b2aeca628f7ae9fb9
aywho <91176452+aywho@users.noreply.github.com>
2023-12-03 22:25:35 +0800

    Update BarChartView.swift

--- BarChartView.swift
+++ BarChartView.swift
@@ -1,1 +1,51 @@
+//
+//  BarChartView.swift
+//  BarChartDemo
+//
+//  Created by Athena Ho on 3/12/2023.
+//
+
+import SwiftUI
+import Charts
+
+struct Course: Identifiable {
+    var category: String
+    var count: Int
+    var id = UUID()
+}
+var courseData: [Course] = [
+    .init(category: "UG5", count: 9),
+    .init(category: "Information Systems", count: 12),
+    .init(category: "Computer Science", count: 14),
+    .init(category: "Free Elective", count: 5)
+]
+
+
+struct BarChartView: View {
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Bar Chart")
+                Chart {
+                    ForEach(courseData) { course in
+                        BarMark(
+                            x: .value("Course Type", course.category),
+                            y: .value("Total Count", course.count)
+                        )
+                    }
+                }
+            }
+            .frame(height: 300)
+            
+            Spacer()
+            
+        }
+    }
+}
+
+#Preview {
+    BarChartView()
+}
+
 
