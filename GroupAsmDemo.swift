ff0f27f98791f14ba48f0c4bcadb9557ba356032
aywho <91176452+aywho@users.noreply.github.com>
2023-12-03 22:26:35 +0800

    Update GroupAsmDemo.swift

--- GroupAsmDemo.swift
+++ GroupAsmDemo.swift
@@ -1,1 +1,22 @@
+//
+//  GroupAsmDemo.swift
+//  Group Assignment Demo 
+//  Data visualization in bar charts and calendar-like timetable
+//
+//  Created by Leo Lai on 1/12/2023.
+//  Updated by Athena Ho on 3/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarChartView()
+            StackedBarChartView()
+            TimetableView(timeSlots: timeslots, myClasses: myClasses)
+        }
+    }
+}
 
